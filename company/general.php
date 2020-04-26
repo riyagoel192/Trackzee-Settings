0e544c68474d5f4ccfbe11d854d60b9bd6f472fe
Riya Goel <55539590+riyagoel192@users.noreply.github.com>
2020-04-26 20:28:30 +0530

    Add files via upload

---
+++ company/general.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style_settings.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style>
+		
+		th:hover
+		{
+			color:red;
+		}
+
+		tr:hover
+		{
+			background-color: lightgreen !important;
+			/*display: block;*/
+		}
+
+		tr:hover td
+		{
+			background-color: lightgreen !important;
+		}
+	
+	</style>
+</head>
+<body style="background-color:#eaeded;">
+
+	<?php
+		include('sample.php');
+	?>
+
+	<div id="main">
+		<div id="company">
+			<p style="margin-left: 8px;color:white;font-size: 18px;padding: 8px;">Company
+
+			<a href="#" style="color:white;"><i class="fa fa-sliders" aria-hidden="true" style="margin-left: 1090px;position: absolute;" title="Settings"></i></a>
+
+			<a href="#" style="color: white;"><i class="fa fa-question" aria-hidden="true" style="margin-left: 1126px;position: absolute;" title="Help"></i></a>
+
+			</p>
+		</div>
+
+		<div id="fortable">
+			
+			<table>
+				
+				<tr>
+					<th>Reseller
+						<img src="https://static.uffizio.com/4.8.33/images/arrow-up.gif" style="width: 12px;height: 11px;">
+					</th>
+
+					<hr style="height: 290px;margin-left: 108px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 120px;">Short Name
+						<img src="https://static.uffizio.com/4.8.33/images/arrow-up.gif" style="width: 12px;height: 11px;">
+					</th>
+					<hr style="height: 290px;margin-left: 235px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 250px;">User Name</th>
+					<hr style="height: 290px;margin-left: 400px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 410px;">Password</th>
+					<hr style="height: 290px;margin-left: 490px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 530px;">Application</th>
+					<hr style="height: 290px;margin-left: 680px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 700px;">User Group</th>
+					<hr style="height: 290px;margin-left: 800px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 810px;">Mobile Number</th>
+					<hr style="height: 290px;margin-left: 910px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 920px;">Last Login Time</th>
+					<hr style="height: 290px;margin-left: 1020px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 1030px;">User Time Zone</th>
+					<hr style="height: 290px;margin-left: 1130px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+
+					<th style="margin-left: 1140px;">Payment Status</th>
+					<hr style="height: 290px;margin-left: 108px;border-left: 1px solid grey;position: absolute;margin-top: -1px;">
+				</tr>
+
+				<tr>
+					<td>Your Company</td>
+					<td style="margin-left: 120px;">Odometer Test</td>
+					<td style="margin-left: 250px;">testodometer@uffizio</td>
+					<td style="margin-left: 410px;">123456</td>
+					<td style="margin-left: 500px;">Trakzee Lite,Trakzee<br style="margin-left: 500px;">Premium,Trakzee Standard</td>
+					<td style="margin-left: 690px;">Odometer Test</td>
+					<td style="margin-left: 840px;">--</td>
+					<td style="margin-left: 920px;">24-03-2020<br style="margin-left: 920px;">04:49 PM</td>
+					<td style="margin-left: 1030px;">Asia/Kolkata</td>
+					<td style="margin-left: 1180px;">--</td>
+				</tr>
+
+				<tr>
+					<td style="margin-top: 120px;">Your Company</td>
+					<td style="margin-left: 120px;margin-top: 120px;">Odometer Test</td>
+					<td style="margin-left: 250px;margin-top: 120px;">testodometer@uffizio</td>
+					<td style="margin-left: 410px;margin-top: 120px;">123456</td>
+					<td style="margin-left: 500px;margin-top: 120px;">Trakzee Lite,Trakzee<br style="margin-left: 500px;">Premium,Trakzee Standard</td>
+					<td style="margin-left: 690px;margin-top: 120px;">Odometer Test</td>
+					<td style="margin-left: 840px;margin-top: 120px;">--</td>
+					<td style="margin-left: 920px;margin-top: 120px;">24-03-2020<br style="margin-left: 920px;">04:49 PM</td>
+					<td style="margin-left: 1030px;margin-top: 120px;">Asia/Kolkata</td>
+					<td style="margin-left: 1180px;margin-top: 120px;">--</td>
+				</tr>
+
+				<tr>
+					<td style="margin-top: 180px;">Your Company</td>
+					<td style="margin-left: 120px;margin-top: 180px;">Odometer Test</td>
+					<td style="margin-left: 250px;margin-top: 180px;">testodometer@uffizio</td>
+					<td style="margin-left: 410px;margin-top: 180px;">123456</td>
+					<td style="margin-left: 500px;margin-top: 180px;">Trakzee Lite,Trakzee<br style="margin-left: 500px;">Premium,Trakzee Standard</td>
+					<td style="margin-left: 690px;margin-top: 180px;">Odometer Test</td>
+					<td style="margin-left: 840px;margin-top: 180px;">--</td>
+					<td style="margin-left: 920px;margin-top: 180px;">24-03-2020<br style="margin-left: 920px;">04:49 PM</td>
+					<td style="margin-left: 1030px;margin-top: 180px;">Asia/Kolkata</td>
+					<td style="margin-left: 1180px;margin-top: 180px;">--</td>
+				</tr>
+
+				<tr>
+					<td style="margin-top: 240px;">Your Company</td>
+					<td style="margin-left: 120px;margin-top: 240px;">Odometer Test</td>
+					<td style="margin-left: 250px;margin-top: 240px;">testodometer@uffizio</td>
+					<td style="margin-left: 410px;margin-top: 240px;">123456</td>
+					<td style="margin-left: 500px;margin-top: 240px;">Trakzee Lite,Trakzee<br style="margin-left: 500px;">Premium,Trakzee Standard</td>
+					<td style="margin-left: 690px;margin-top: 240px;">Odometer Test</td>
+					<td style="margin-left: 840px;margin-top: 240px;">--</td>
+					<td style="margin-left: 920px;margin-top: 240px;">24-03-2020<br style="margin-left: 920px;">04:49 PM</td>
+					<td style="margin-left: 1030px;margin-top: 240px;">Asia/Kolkata</td>
+					<td style="margin-left: 1180px;margin-top: 240px;">--</td>
+				</tr>
+			</table>
+
+			<div id="line"></div>
+
+			<div id="last">
+				
+				<div id="back">
+					<a href="#" style="color:white;"><i class="fa fa-arrow-left fa-2x" aria-hidden="true" style="color:white;margin-top: 6px;margin-left: 10px;" title="Back"></i></a>
+				</div>
+
+				<div id="reset">
+					<a href="#" style="color:#234292;"><i class="fa fa-refresh fa-2x" aria-hidden="true" style="color:#234292;margin-top: 6px;margin-left: 10px;" title="Reset"></i></a>
+				</div>
+
+				<div id="xls">
+					<a href="#"><img src="xls.jpg" title="Excel Download" style="height: 50px;width: 70px;"></a>
+				</div>
+
+				<form>
+					<input type="text" name="val1" style="margin-left: 450px;position: absolute;margin-top: -33px;border: 1px solid grey;" />
+		
+					<div style="margin-left: 600px;position: absolute;margin-top: -33px;border: 1px solid grey;width: 140px;height: 24px;" 
+					onclick="func()">
+						<p style="position: absolute;margin-left: 10px;">All</p>
+					</div>
+
+					<div id="size">
+						<ul style="list-style-type: none;">
+							<li style="color:black;position: absolute;margin-left: -30px;">All</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">Reseller</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">Short Name</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">User Name</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">Password</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">Application</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">User Group</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">Mobile Number</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">Last Login Time</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">User Time Zone</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">Payment Status</li><br>
+						</ul>
+					</div>
+					<a href=""><i class="fa fa-search" aria-hidden="true" style="margin-left: 748px;position: absolute;margin-top: -30px;"></i></a>
+				</form>
+
+				<div id="pages" onclick="show()">
+
+					<i class="fa fa-bars" aria-hidden="true" style="margin-left: 4px;color:#234292;"></i>
+					<p style="margin-left: 20px;margin-top: -20px;position: absolute;">20</p>
+					<img src="https://static.uffizio.com/4.8.33/images/arrow-up.gif" style="width: 12px;height: 11px;margin-left: 40px;margin-top: -50px;">
+					<img src="https://static.uffizio.com/4.8.33/images/arrow-down.gif" style="width: 12px;height: 11px;margin-left: 40px;margin-top: -75px;">
+
+					<div id="showdiv">
+						
+						<ul style="list-style-type: none;">
+							<li style="color:black;position: absolute;margin-left: -30px;">10</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">20</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">30</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">40</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">50</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">100</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">200</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">300</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">400</li><br>
+							<li style="color:black;position: absolute;margin-left: -30px;">500</li><br>
+							<li style="color:black;position: absolute;margin-left: -35px;">1000</li><br>
+						</ul>
+
+					</div>
+
+				</div>
+				<a href="#"><i class="fa fa-chevron-left" aria-hidden="true" style="position: absolute;margin-left: 1100px;margin-top: -28px;"></i></a>
+
+				<div id="pages" style="margin-left: 1115px;" onclick="one()">
+
+					<i class="fa fa-file-text" aria-hidden="true" style="margin-left: 4px;color:#234292;"></i>
+					<p style="margin-left: 25px;margin-top: -20px;position: absolute;">1</p>
+					<img src="https://static.uffizio.com/4.8.33/images/arrow-up.gif" style="width: 12px;height: 11px;margin-left: 40px;margin-top: -50px;">
+					<img src="https://static.uffizio.com/4.8.33/images/arrow-down.gif" style="width: 12px;height: 11px;margin-left: 40px;margin-top: -75px;">
+
+					<div id="showone">
+						<ul style="list-style-type: none;">
+							<li style="color:black;position: absolute;margin-left: -30px;">1</li><br>
+						</ul>
+					</div>
+
+				</div>
+
+				<a href="#"><i class="fa fa-chevron-right" aria-hidden="true" style="position: absolute;margin-left: 1180px;margin-top: -28px;"></i></a>
+
+				<div id="content">
+					<p>1-4 (4)</p>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+</body>
+</html>
+
+<script>
+	function func()
+	{
+		size.style.display="block";
+	}
+
+	function show()
+	{
+		showdiv.style.display="block";
+	}
+
+	function one()
+	{
+		showone.style.display="block";
+	}
+</script>
